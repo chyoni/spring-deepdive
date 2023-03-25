6fbef97215986e1f1dad181905e43c025111d5c7
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-03-25 12:31:46 +0900

    Thymeleaf javascript

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -57,6 +57,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUser(model);
+        return "basic/javascript";
+    }
+
     private void addUser(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
